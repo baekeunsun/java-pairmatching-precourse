1bca9de57b0b00d89b32c16c0efe05b49cdbf0be
baekeunsun <eunsun5460@gmail.com>
2022-11-27 16:00:45 +0900

    feat(domain/PairMatching): 랜덤으로 섞인 페어 목록에서 페어 매칭
    - (PairMatching/match): 조건에 따라 매치
    - (PairMatching/matchEven): 짝수일 경우 랜덤으로 섞은 리스트에서 둘씩 매치
    - (PairMatching/printMatching): 매치된 값들 출력(향후 OutputView로 이동)

--- src/main/java/pairmatching/domain/PairMatching.java
+++ src/main/java/pairmatching/domain/PairMatching.java
@@ -3,14 +3,51 @@ package pairmatching.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static pairmatching.utils.Course.BACKEND;
+
 public class PairMatching {
+    public static List<String> shuffledBackendCrew = new ArrayList<>();
+    public static List<String> shuffledFrontendCrew = new ArrayList<>();
+
     public PairMatching() throws IOException {
         List<String> backendCrew = MakeNameList.makeBackendList();
-        List<String> shuffledBackendCrew = Randoms.shuffle(backendCrew);
+        this.shuffledBackendCrew = Randoms.shuffle(backendCrew);
 
         List<String> frontendCrew = MakeNameList.makeFrontendList();
-        List<String> shuffledFrontendCrew = Randoms.shuffle(frontendCrew);
+        this.shuffledFrontendCrew = Randoms.shuffle(frontendCrew);
+    }
+    public void match(String endType, String levelType, String missionType) {
+        List<String> shuffledCrew = shuffledBackendCrew;
+        List<List<String>> resultCrew = new ArrayList<>();
+
+        System.out.println(shuffledCrew);
+
+        if (shuffledCrew.size()%2 == 0) {
+            System.out.println("짝수");
+            resultCrew = matchEven(shuffledCrew);
+        }
+        printMatching(resultCrew);
+    }
+    private List<List<String>> matchEven(List<String> shuffledCrew) {
+        List<List<String>> resultCrew = new ArrayList<>();
+        for (int i=0; i < shuffledCrew.size(); i +=2) {
+            System.out.println(i);
+            resultCrew.add(Arrays.asList(shuffledCrew.get(i),shuffledCrew.get(i+1)));
+        }
+        return resultCrew;
+    }
+    public void printMatching(List<List<String>> resultCrew) {
+        for (int i=0; i <resultCrew.size(); i++) {
+            String str = resultCrew.get(i).toString();
+            str = str.replace(","," :");
+            str = str.replace("[","");
+            str = str.replace("]","");
+            System.out.println(str);
+        }
     }
 }
